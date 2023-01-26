8722e3eb76f1acb884c6698d156199878d131023
jefferson <jefferson.ferraz@live.com>
2023-01-26 12:02:02 +0100

    Update User.class.php

--- src/classes/User.class.php
+++ src/classes/User.class.php
@@ -8,33 +8,6 @@ class User {
     public $email;
     public $mobile;
 
-    // public $link;
-
-    
-    // public function __construct($databaseLink) {
-    //     $this->link = $databaseLink;
-    // }
-
-    // //adding user
-    // public function insertUser($first_name, $last_name, $email, $mobile) {
-
-    //     $conn = new ConnectionDb;
-        
-    //     //preparing the statement for SQL command
-    //     $stmt = $conn->connectDb()->prepare("INSERT INTO user (first_name, last_name, email, mobile)
-    //     VALUES (:first_name, :last_name, :email, :mobile)");
-
-    //     $stmt->bindParam(':first_name', $first_name);
-    //     $stmt->bindParam(':last_name', $last_name);
-    //     $stmt->bindParam(':email', $email);
-    //     $stmt->bindParam(':mobile', $mobile);
-
-    //     $stmt->execute();
-
-    //     echo "<p>Du wurdest erfolgreich registriert.</p>";
-
-    // }
-
     public function setFirstName($first_name) {
         $this->first_name = $first_name;
 
